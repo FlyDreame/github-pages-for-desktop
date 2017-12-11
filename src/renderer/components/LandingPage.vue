279c4c6c427361d4eb875224569d54826deb1b23
FlyDreame <flydreame@foxmail.com>
2017-12-11 19:56:39 +0800

    添加sass支持

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -21,8 +21,7 @@
         </div>
         <div class="doc">
           <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+          <el-button type="primary" @click="open('https://vuejs.org/v2/guide/')">Vue.js</el-button>
         </div>
       </div>
     </main>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,6 +6,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import './theme/element-variables.scss'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
