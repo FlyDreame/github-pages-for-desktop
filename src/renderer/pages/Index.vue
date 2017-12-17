4f98062db9c314980dc8089c52b6258554b7a4a9
FlyDreame <flydreame@foxmail.com>
2017-12-17 13:00:53 +0800

    '搭建主要样式'

--- src/renderer/pages/Index.vue
+++ src/renderer/pages/Index.vue
@@ -6,12 +6,14 @@
         <span>GPage</span>
       </div>
       <div class="page-index-btn-group">
-        <el-button type="primary"><i class="el-icon-minus"></i></el-button>
-        <el-button type="primary"><i class="icon icon-square"></i></el-button>
-        <el-button type="primary"><i class="el-icon-close"></i></el-button>
+        <el-button type="text"><i class="el-icon-minus"></i></el-button>
+        <el-button type="text"><i class="icon icon-square"></i></el-button>
+        <el-button type="text"><i class="el-icon-close"></i></el-button>
       </div>
     </header>
-    <router-view></router-view>
+    <div class="gpage-index__main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
